a0557f69bcd3156f4fd89b8f01280ccd94a28902
Luiza <luizamazo@hotmail.com>
2018-11-22 05:21:39 -0200

    app.vue certo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    
-          <nav-bar></nav-bar>
-          <hr>   
-=======
     <div class="container ">
         <div class="row">
           <div class="col-sm-12">
-           <h1> {{auth}} | {{role}}</h1>
+           <h1> {{username}}</h1>
             <router-link to="/login">Login</router-link> |
             <router-link to="/novo-aluno">Register</router-link> |
             <router-link to="/logout">Logout</router-link> 
@@ -17,28 +12,24 @@
           <hr>
         <div class="row">
           <div class="col-sm-12">
->>>>>>> 9837a04dc082fdce04f671644d73d29fceb5c47e
             <router-view></router-view>
-            <footer></footer>
+          </div>
+        </div>
+    </div>
   </div>
-
 </template>
-<script>
-    import Footer from './components/Footer.vue';   
-    import NavBar from './components/NavBar.vue';
-    export default {
-        name: 'app',
-        components: {NavBar,Footer}
-    }
-</script>
+
 
 <script>
 export default {
     data(){
       return{
-        auth: localStorage.getItem('username'),
-        role: localStorage.getItem('role')
+        username: ''
       }
+    },
+    created(){
+      const username = localStorage.getItem('username');
+      this.username = username
     }
 }
 </script>
@@ -49,4 +40,3 @@ export default {
     margin: 30px;
   }
 </style>
-
--- src/components/NovaSolicitacao.vue
+++ src/components/NovaSolicitacao.vue
@@ -3,65 +3,65 @@
         <div class="panel-heading"><h2>Solicitação de estágio</h2></div>
         <div class="panel-body">
               <div class="row justify-content-center">
-    <div class="col-sm-6">
-        <form>
-            <p style="color: red;"><strong>Obs: Só faça a solicitação caso todos os termos tenham sido previamente acordados.</strong></p>
-          
-            <div class="form-group">
-                <label for="nome">Nome Completo</label>
-                <input type="nome" id="nome" name="nome" 
-                class="form-control" v-model="nome">
-            </div>
-        
-            <div class="form-group"> 
-                <label for="nascimento">Data de Nascimento</label>
-                <input type="date" class="form-control" name="nascimento" v-model="nascimento">
-            </div>
-            <div class="form-group"> 
-                <label for="cpf">CPF</label>
-                <input type="text" class="form-control" name="cpf" placeholder="Ex. 999.999.999-99" v-model="cpf">
-            </div>
-            <div class="form-group">
-                <label for="rg">RGA</label>
-                <input type="text" class="form-control" name="rg" placeholder="RG" v-model="rg">
-            </div>
-            <div class="form-group"> 
-                <label for="telefone">Telefone</label>
-                <div class="form-row">
-                    <div class="col-6">
-                        <input type="text" class="form-control" name="fixo" placeholder="Fixo" v-model="fixo">
-                    </div>
-                    <div class="col">
-                        <input type="text" class="form-control" name="celular" placeholder="Celular" v-model="celular"> 
-                    </div>
+                <div class="col-sm-6">
+                    <form>
+                        <p style="color: red;"><strong>Obs: Só faça a solicitação caso todos os termos tenham sido previamente acordados.</strong></p>
+                    
+                        <div class="form-group">
+                            <label for="nome">Nome Completo</label>
+                            <input type="nome" id="nome" name="nome" 
+                            class="form-control" v-model="nome">
+                        </div>
+                    
+                        <div class="form-group"> 
+                            <label for="nascimento">Data de Nascimento</label>
+                            <input type="date" class="form-control" name="nascimento" v-model="nascimento">
+                        </div>
+                        <div class="form-group"> 
+                            <label for="cpf">CPF</label>
+                            <input type="text" class="form-control" name="cpf" placeholder="Ex. 999.999.999-99" v-model="cpf">
+                        </div>
+                        <div class="form-group">
+                            <label for="rg">RGA</label>
+                            <input type="text" class="form-control" name="rg" placeholder="RG" v-model="rg">
+                        </div>
+                        <div class="form-group"> 
+                            <label for="telefone">Telefone</label>
+                            <div class="form-row">
+                                <div class="col-6">
+                                    <input type="text" class="form-control" name="fixo" placeholder="Fixo" v-model="fixo">
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" name="celular" placeholder="Celular" v-model="celular"> 
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="form-group"> 
+                            <label for="estagioInicio">Data de Início do Estágio</label>
+                            <input type="date" class="form-control" name="estagioInicio" v-model="estagioInicio">
+                        </div>
+                        <div class="form-group"> 
+                            <label for="estagioFinal">Data de Encerramento</label>
+                            <input type="date" class="form-control" name="estagioFinal" v-model="estagioFinal">
+                        </div>
+                        <div class="form-group">
+                            <label for="tarefas">Planejamento de tarefas</label>
+                            <textarea class="form-control" id="tarefas" rows="5" v-model="tarefas"></textarea>
+                        </div>
+                        <label for="termo">Termo de Compromisso</label>
+                        <div class="form-group">
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="file">
+                                <label class="custom-file-label" for="customFile">Selecione seu documento</label>
+                            </div>
+                        </div>
+                    
+                        <button @click.prevent="register" type="submit" class="btn btn-warning">Enviar Solicitação</button>
+                        <router-link to="/dashboard/aluno" class="btn btn-danger">Cancelar</router-link>
+                    </form>
                 </div>
             </div>
-            <hr>
-            <div class="form-group"> 
-                <label for="estagioInicio">Data de Início do Estágio</label>
-                <input type="date" class="form-control" name="estagioInicio" v-model="estagioInicio">
-            </div>
-            <div class="form-group"> 
-                <label for="estagioFinal">Data de Encerramento</label>
-                <input type="date" class="form-control" name="estagioFinal" v-model="estagioFinal">
-            </div>
-             <div class="form-group">
-                <label for="tarefas">Planejamento de tarefas</label>
-                <textarea class="form-control" id="tarefas" rows="5" v-model="tarefas"></textarea>
-            </div>
-            <label for="termo">Termo de Compromisso</label>
-             <div class="form-group">
-                 <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="file">
-                    <label class="custom-file-label" for="customFile">Selecione seu documento</label>
-                 </div>
-            </div>
-           
-            <button @click.prevent="register" type="submit" class="btn btn-warning">Enviar Solicitação</button>
-            <router-link to="/dashboard/aluno" class="btn btn-danger">Cancelar</router-link>
-        </form>
-    </div>
-</div>
         </div>        
     </div>
 </template>
